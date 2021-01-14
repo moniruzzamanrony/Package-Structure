d87c2e4d777d26c75064fd0c4da309257948a53e
monirozzaman <moniruzzaman35-223@diu.edu.bd>
2021-01-14 12:03:19 +0600

    initial commit

---
+++ src/main/java/com/itvillage/packagestructure/PackageStructureApplication.java
@@ -0,0 +1,13 @@
+package com.itvillage.packagestructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itvillage/packagestructure/PackageStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvillage.packagestructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
